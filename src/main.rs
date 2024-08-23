0708cb9d44f30626e98ecce4184d476df0c7b657
kuu <akumakumu@gmail.com>
2024-08-23 12:50:44 +0700

    add sqlx

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,23 @@ use axum::{
 };
 use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::Row;
 
 #[tokio::main]
 async fn main() {
     dotenv().expect(".env not found");
 
+    let pg_url = env::var("DATABASE_URL"). expect("DATABASE_URL must be SET");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&pg_url).await
+        .expect("failed to create database pool");
+
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
