473a4e102fdaa09a882ac11ad37ed6358e90f743
kuu <akumakumu@gmail.com>
2024-08-23 11:28:11 +0700

    json

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
+use dotenvy::dotenv;
+use std::env;
 use axum::{
+    Router,
     routing::{get, post},
     http::StatusCode,
-    Json, Router,
+    Json
 };
 use serde::{Deserialize, Serialize};
+use serde_json::{Value, json};
 
 #[tokio::main]
 async fn main() {
+    dotenv().expect(".env not found");
+
     let app = Router::new()
-        .route("/", get(hello));
+        .route("/", get(root));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn hello() -> &'static str {
-    "hello mom"
+async fn root() -> Json<Value> {
+    Json(json!({"data": "hello mom"}))
 }
