b6bb2187fa29ea578bbdcb4b90cdc463a6259271
kuu <akumakumu@gmail.com>
2024-08-23 11:12:09 +0700

    change from actix to axum

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,20 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use dotenvy::dotenv;
-use std::env;
+use axum::{
+    routing::{get, post},
+    http::StatusCode,
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    let nihao = env::var("HELLO").expect("HELLO not SET");
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello));
 
-    HttpResponse::Ok().body(nihao)
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey!")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenv().expect(".env file not found");
-
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+async fn hello() -> &'static str {
+    "hello mom"
 }
